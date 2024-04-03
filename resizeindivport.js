54c3a498cffcb5dcad12b7119fe8a56e33db993c
biffmalibu <bradfordtorpey@gmail.com>
2024-04-02 22:32:49 -0400

    test 4 or 5??

--- resizeindivport.js
+++ resizeindivport.js
@@ -3,16 +3,47 @@ window.addEventListener('DOMContentLoaded', function() {
     var main = document.querySelector('main');
     var containerHeight = container.offsetHeight;
     var viewerHeight = window.innerHeight;
-    var difference = viewerHeight - containerHeight;
+    var headerHeight = document.querySelector('header').offsetHeight;
+    var footerHeight = document.querySelector('footer').offsetHeight;
+    var navbarHeight = document.querySelector('.navbar').offsetHeight;
+    var aHeight = document.querySelector('a').offsetHeight;
+    var totalHeight = headerHeight + footerHeight + navbarHeight + aHeight;
+    var difference = viewerHeight - totalHeight;
     console.log(difference);
-    main.style.height = containerHeight  + difference/2 - 100 + 'px';
+    main.style.height = difference + 'px';
 });
+
 window.addEventListener('resize', function() {
     var container = document.querySelector('.container');
     var main = document.querySelector('main');
     var containerHeight = container.offsetHeight;
     var viewerHeight = window.innerHeight;
-    var difference = viewerHeight - containerHeight;
+    var headerHeight = document.querySelector('header').offsetHeight;
+    var footerHeight = document.querySelector('footer').offsetHeight;
+    var navbarHeight = document.querySelector('.navbar').offsetHeight;
+    var aHeight = document.querySelector('a').offsetHeight;
+    var totalHeight = headerHeight + footerHeight + navbarHeight + aHeight;
+    var difference = viewerHeight - totalHeight;
     console.log(difference);
-    main.style.height = containerHeight  + difference/2 - 100 + 'px';
+    main.style.height = difference + 'px';
 });
+
+// window.addEventListener('DOMContentLoaded', function() {
+//     var container = document.querySelector('.container');
+//     var main = document.querySelector('main');
+//     var containerHeight = container.offsetHeight;
+//     var viewerHeight = window.innerHeight;
+//     var difference = viewerHeight - containerHeight;
+//     console.log(difference);
+//     main.style.height = containerHeight  + difference/2 - 100 + 'px';
+// });
+// window.addEventListener('resize', function() {
+//     var container = document.querySelector('.container');
+//     var main = document.querySelector('main');
+//     var containerHeight = container.offsetHeight;
+//     var viewerHeight = window.innerHeight;
+//     var difference = viewerHeight - containerHeight;
+//     console.log(difference);
+//     main.style.height = containerHeight  + difference/2 - 100 + 'px';
+// });
+
